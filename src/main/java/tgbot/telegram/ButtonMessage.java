28aff4bcfb6470c0d49c9e8040f3fba907ca7454
Pavel Bedin <bedinpasha@mail.ru>
2022-12-15 23:15:34 +0500

    add class ButtonMessage

---
+++ src/main/java/tgbot/telegram/ButtonMessage.java
@@ -0,0 +1,94 @@
+package tgbot.telegram;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ButtonMessage {
+
+    public final String CAFE_BUTTON = "CAFE_BUTTON";
+
+    public final String PARK_BUTTON = "PARK_BUTTON";
+
+    public final String BAR_BUTTON = "BAR_BUTTON";
+
+    public final String PEDESTRIAN_BUTTON = "PEDESTRIAN_BUTTON";//Пешеходный маршрут
+
+    public final String JAM_BUTTON = "JAM_BUTTON";//На машине
+
+    public final String BICYCLE_BUTTON = "BICYCLE_BUTTON";
+    private final InlineKeyboardMarkup markupInLine;
+    private final List<List<InlineKeyboardButton>> rowsInLine;
+    private final List<InlineKeyboardButton> rowInLine;
+
+    ButtonMessage(){
+
+        markupInLine = new InlineKeyboardMarkup();
+        rowsInLine = new ArrayList<>();
+        rowInLine = new ArrayList<>();
+
+    }
+
+    public void Place(SendMessage message) {
+        var cafeButton = new InlineKeyboardButton();
+
+        cafeButton.setText("Кафе");
+        cafeButton.setCallbackData(CAFE_BUTTON);
+
+        var parkButton = new InlineKeyboardButton();
+
+        parkButton.setText("Парк");
+        parkButton.setCallbackData(PARK_BUTTON);
+
+        var barButton = new InlineKeyboardButton();
+
+        barButton.setText("Бар");
+        barButton.setCallbackData(BAR_BUTTON);
+
+
+        rowInLine.add(cafeButton);
+        rowInLine.add(parkButton);
+        rowInLine.add(barButton);
+
+        rowsInLine.add(rowInLine);
+
+        markupInLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInLine);
+    }
+
+    public void routeType(SendMessage message) {
+        var pedestrianButton = new InlineKeyboardButton();
+
+        pedestrianButton.setText("Пешком");
+        pedestrianButton.setCallbackData(PEDESTRIAN_BUTTON);
+
+        var jamButton = new InlineKeyboardButton();
+
+        jamButton.setText("На машине");
+        jamButton.setCallbackData(JAM_BUTTON);
+
+        var bicycleButton = new InlineKeyboardButton();
+
+        bicycleButton.setText("На велике");
+        bicycleButton.setCallbackData(BICYCLE_BUTTON);
+
+        rowInLine.add(pedestrianButton);
+        rowInLine.add(jamButton);
+        rowInLine.add(bicycleButton);
+
+        rowsInLine.add(rowInLine);
+
+        markupInLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInLine);
+    }
+
+    public void resetRows(){
+        rowInLine.clear();
+        rowsInLine.clear();
+    }
+
+}
